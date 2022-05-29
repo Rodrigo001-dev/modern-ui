5e642c3f7409aa7b978ce0a2279ce63bce0b9747
Rodrigo <rodrigorael53@gmail.com>
2022-05-29 18:11:32 -0300

    animating the Card component using react-native-animatable

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,11 +6,19 @@ import { Container, Image, Title, Foundation } from './styles';
 
 export function Card() {
   return (
-    <Container>
+    <Container
+      animation="slideInDown"
+      duration={2000} // 2 seconds
+    >
       <Image 
         source={unitedImg}
         // o contain vai deixar a imagem ajustada em relação o Container
         resizeMode="contain"
+        animation="pulse"
+        delay={100}
+        duration={600} // 600ms
+        direction="alternate"
+        iterationCount="infinite"
       />
 
       <Title>Manchester United</Title>
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components/native";
-import theme from "../../theme";
+// essa biblioteca é para deixar as fontes responsivas
+import { RFPercentage } from 'react-native-responsive-fontsize';
+// essa biblioteca é para animações, ela já vem com algumas animações prontas
+import * as Animatable from 'react-native-animatable';
 
-export const Container = styled.View`
+export const Container = styled(Animatable.View)`
   align-items: center;
 
   width: 180px;
@@ -11,7 +14,7 @@ export const Container = styled.View`
   border-radius: 10px;
 `;
 
-export const Image = styled.Image`
+export const Image = styled(Animatable.Image)`
   margin-top: -50px;
   width: 150px;
   height: 150px;
@@ -20,9 +23,9 @@ export const Image = styled.Image`
 export const Title = styled.Text`
   color: ${({ theme }) => theme.COLORS.BACKGROUND};
   font-family: ${({ theme }) => theme.FONTS.BOLD};
-  font-size: 18px;
+  font-size: ${RFPercentage(2.5)}px;
 
-  margin-top: 24px;
+  margin-top: ${RFPercentage(5)};
 `;
 
 export const Foundation = styled.Text`
